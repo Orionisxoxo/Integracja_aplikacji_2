8e93ed476ab215bfb489ac5ac6128dec161c56b8
Iwona <aikodei@gmail.com>
2019-12-04 17:06:03 +0100

    z zajęć

--- monolith/views/gitlab.py
+++ monolith/views/gitlab.py
@@ -1,12 +1,18 @@
 from flask import Blueprint, jsonify
-from monolith.background import fetch_all_projects
-
+from monolith.background import fetch_all_projects, fetch_all_groups
 
 gitlab = Blueprint('gitlab', __name__)
 
 
-@gitlab.route('/fetch')
-def fetch_runs():
+@gitlab.route('/fetch/Projects')
+def fetch_Projects():
     res = fetch_all_projects.delay()
+    res.wait()
+    return jsonify(res.result)
+
+
+@gitlab.route('/fetch/Groups')
+def fetch_Groups():
+    res = fetch_all_groups.delay()
     res.wait()
     return jsonify(res.result)
